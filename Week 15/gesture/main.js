945f1f0d2f1c5137ab770b2b601de3950f44af0f
att286 <att286@163.com>
2021-04-18 23:24:47 +0800

    15周

--- Week 15/gesture/main.js
+++
@@ -1,5 +0,0 @@
-import { enableGesture } from './gesture.js'
-enableGesture(document.documentElement)
-document.documentElement.addEventListener('tap', () => {
-  console.log('tap event trigger!')
-})
